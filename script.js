1d5cb6b08a14a66c7dd2a78f74188d43210c817e
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-04-05 03:59:00 +0200

    Fix linter errors on javascript file3

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const crossMenu = document.querySelector('.fa-xmark');
 const mobileMenu = document.querySelector('.mobile-menu');
 barImage.addEventListener('click', () => {
   mobileMenu.style.display = 'block';
-})
+});
 crossMenu.addEventListener('click', () => {
   mobileMenu.style.display = 'none';
-})
-document.querySelectorAll('.hamburger-nav-list').forEach
-((n) => n.addEventListener('click', () => {
+});
+document.querySelectorAll('.hamburger-nav-list').forEach((n) => n.addEventListener('click', () => {
   mobileMenu.style.display = 'none';
-}))
+}));
