150a3555f137c27044da639c91c1cadd11894740
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-04-05 03:49:10 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-let barImage = document.querySelector(".menu");
-let crossMenu = document.querySelector(".fa-xmark");
-let mobileMenu = document.querySelector(".mobile-menu");
-
-
-barImage.addEventListener("click" , function() {
-    mobileMenu.style.display = "block";
+const barImage = document.querySelector('.menu');
+const crossMenu = document.querySelector('.fa-xmark');
+const mobileMenu = document.querySelector('.mobile-menu');
+barImage.addEventListener('click', () => {
+  mobileMenu.style.display = 'block';
 })
-
-crossMenu.addEventListener("click", function() {
-    mobileMenu.style.display = "none";
+crossMenu.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
 })
-
-
-document.querySelectorAll(".hamburger-nav-list").forEach
-(n => n.addEventListener("click", () => {
-    mobileMenu.style.display = "none";
+document.querySelectorAll('.hamburger-nav-list').forEach
+((n) => n.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
 }))
-
